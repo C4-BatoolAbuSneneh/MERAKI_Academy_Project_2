64f1565abc015a8a4ea595aba17966555147570b
BatoolAbuSneneh <batoolabusneneh56@gmail.com>
2021-12-10 09:36:45 -0800

    edit html

---
+++ main.js
@@ -0,0 +1,17 @@
+const body = $("body");
+const header = $("<h1>To Do List </h1>");
+header.appendTo(body);
+const list = $("<ul></ul>");
+list.appendTo(body);
+const inputName = $("<input></input>");
+const button = $("<button>Add To List</button>");
+inputName.appendTo(body);
+button.appendTo(body);
+let todo=[];
+const addItems = () => {
+   todo.forEach((element, index) => {
+
+   
+  })
+    li.appendTo(list)
+}
