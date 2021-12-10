df8d5688187f09e5647a7d70c5899d4beca9ad78
BatoolAbuSneneh <batoolabusneneh56@gmail.com>
2021-12-10 11:14:00 -0800

    add jquery

--- main.js
+++ main.js
@@ -3,15 +3,29 @@ const header = $("<h1>To Do List </h1>");
 header.appendTo(body);
 const list = $("<ul></ul>");
 list.appendTo(body);
-const inputName = $("<input></input>");
-const button = $("<button>Add To List</button>");
+const tasks = $("<p>Tasks :</p>")
+tasks.appendTo(body)
+const inputName = $("</input>");
+const button = $("<button >Add To List</button>");
+button.id = ("add")
 inputName.appendTo(body);
 button.appendTo(body);
 let todo=[];
 const addItems = () => {
-   todo.forEach((element, index) => {
-
-   
+   $(document).ready( () => {
+    $("#btn").click(function(){
+    $("li").append("<p>Tasks :</p>");
+  });
+  // $("#btn").click( () => {
+  //   $("#btn").append("<p>Tasks :</p>")
+  // })
   })
-    li.appendTo(list)
+    // li.appendTo(list)
 }
+
+
+
+
+//////////////////////////////////////////
+
+
