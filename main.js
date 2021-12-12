858b0066659ab0fde9f9d5b7ace1adc18245436b
BatoolAbuSneneh <batoolabusneneh56@gmail.com>
2021-12-11 21:16:12 -0800

    js and css

--- main.js
+++ main.js
@@ -1,70 +1,64 @@
-const body = $("body");
-const header = $("<h1></h1>")
-const listItem = $("<ul></ul>");
-const button = $("<button></button>");
-const inputToDo = $("<input/>");
-header.text("To Do List :");
-button.text("Add To List");
+$(() => {
+  const body = $("body");
+  const header = $("<h1></h1>")
+  const listItem = $("<ul></ul>");
+  const button = $("<button></button>");
+  const inputToDo = $("<input/>");
+  header.text("To Do List :");
+  button.text("Add To List");
 
-const arr = [];
+  header.appendTo(body);
+  listItem.appendTo(header);
+  inputToDo.appendTo(header);
+  button.appendTo(header);
 
-header.appendTo(body);
-listItem.appendTo(header);
-inputToDo.appendTo(header);
-button.appendTo(header);
+  let list = JSON.parse(localStorage.getItem("ToDos") || "[]");
 
-let list = JSON.parse(localStorage.getItem("ToDos"));
-// console.log(list)
-const addList = () => {
- list.forEach((element) => {
-    const toDo = $("<li></li>");
-    const deleteButton = $(`<button class="delete";><i class="fas fa-trash-alt"></i></button>`);
-    const updateButton = $(`<button class="edit";><i class="far fa-edit"></i></button>`);
-    const updateToDo = $("<input/>");
-    toDo.text(element);
-    listItem.append(toDo);
-    toDo.append(deleteButton);
-    toDo.append(updateButton);
-    toDo.append(updateToDo);
+  const addList = () => {
+   list.forEach((element) => {
+      const toDo = $("<li></li>");
+      const deleteButton = $(`<button class="delete";><i class="fas fa-trash-alt"></i></button>`);
+      const updateButton = $(`<button class="edit";><i class="far fa-edit"></i></button>`);
+      const updateToDo = $("<input/>");
+      toDo.text(element);
+      listItem.append(toDo);
+      toDo.append(deleteButton);
+      toDo.append(updateButton);
+      toDo.append(updateToDo);
 
-    deleteButton.on("click", () => {
-      list.forEach((toDo, i) => {
-        if (element === toDo) {
-          list.splice(i, 1);
-        }
-      });
-      toDo.remove();
-    });
-
-    updateButton.on("click", () => {
-      list.forEach((toDo, i) => {
-        if (element === toDo) {
-          list[i] = updateToDo.val();
-        }
+      deleteButton.on("click", () => {
+        list.forEach((toDo, i) => {
+          if (element === toDo) {
+            list.splice(i, 1);
+          }
+        });
+        toDo.remove();
       });
 
-      listItem.html("");
-      addList();
+      updateButton.on("click", () => {
+        list.forEach((toDo, i) => {
+          if (element === toDo) {
+            list[i] = updateToDo.val();
+          }
+        });
+
+        listItem.html("");
+        addList();
+      });
     });
+  };
+  const addToList = () => {
+    list.push(inputToDo.val());
+    localStorage.setItem("ToDos", JSON.stringify(list));
+    localStorage.removeItem("ToDos", JSON.stringify(list));
+    localStorage.setItem("ToDos", JSON.stringify(list));
+    listItem.html("");
+    addList();
+  };
+
+  button.on("click", () => {
+    addToList();
   });
-};
-const addToList = () => {
-  list.push(inputToDo.val());
-  localStorage.setItem("ToDos", JSON.stringify(arr));
-  //  JSON.parse(localStorage.removeItem("toDos"));
-  listItem.html("");
+
   addList();
-};
-
-button.on("click", () => {
-  addToList();
 });
-
-addList();
-
-// Window.localStorage
-// localStorage.getItem("Key")
-
-// list.push(input);
-  // window.localStorage.removeItem( "ToDos");
-  // localStorage.removeItem = null;
