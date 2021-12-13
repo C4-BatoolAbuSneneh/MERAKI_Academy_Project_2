7fbb8dd20ad9806441d776e1ac3ebaea440ac0fb
BatoolAbuSneneh <batoolabusneneh56@gmail.com>
2021-12-13 11:31:14 -0800

    add line in the text

--- main.js
+++ main.js
@@ -13,21 +13,24 @@ $(() => {
   inputToDo.appendTo(header);
   button.appendTo(header);
 
-  let list = JSON.parse(localStorage.getItem("ToDos") || "[]");
-
+  const list = JSON.parse(localStorage.getItem("ToDos")) || [];
   const addList = () => {
+     console.log(list);
    list.forEach((element) => {
+    
       const toDo = $("<li></li>");
       const deleteButton = $(`<button class="delete";><i class="fas fa-trash-alt"></i></button>`);
+      const line = $(`<button class="line";><i class="fas fa-check"></i></button>`);
       const updateButton = $(`<button class="edit";><i class="far fa-edit"></i></button>`);
       const updateToDo = $("<input/>");
       toDo.text(element);
       listItem.append(toDo);
       toDo.append(deleteButton);
+      toDo.append(line);
       toDo.append(updateButton);
       toDo.append(updateToDo);
 
-      deleteButton.on("click", () => {
+      deleteButton.on("click", () => {   
         list.forEach((toDo, i) => {
           if (element === toDo) {
             list.splice(i, 1);
@@ -35,7 +38,6 @@ $(() => {
         });
         toDo.remove();
     localStorage.setItem("ToDos", JSON.stringify(list));
-
       });
 
       updateButton.on("click", () => {
@@ -48,10 +50,12 @@ $(() => {
         listItem.html("");
         addList();
         localStorage.setItem("ToDos", JSON.stringify(list));
-
       });
+      line.on("click", () =>{
+        toDo.css('text-decoration-line', 'line-through')
+})
     });
-  };
+  }; 
   const addToList = () => {
     list.push(inputToDo.val());
     localStorage.setItem("ToDos", JSON.stringify(list));
@@ -67,3 +71,24 @@ $(() => {
   addList();
 });
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//       let list = document.querySelector('ul');
+// list.addEventListener('click', function(ev) {
+//   if (ev.target.tagName === 'list') {
+//     ev.target.classList.toggle('checked');
+//   }
+// }, false);
